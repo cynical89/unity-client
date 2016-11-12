f81958deab90d175fb7e208d71f04accaf76bae7
Anthony Michelizzi <cynical89@gmail.com>
2016-11-12 12:46:27 -0800

    Starter code

---
+++ Assets/scripts/startArgs.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class startArgs : MonoBehaviour
+{
+    private static string _cmdInfo = string.Empty;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    string[] arguments = Environment.GetCommandLineArgs();
+
+	    foreach (string arg in arguments)
+	    {
+	        _cmdInfo += arg.ToString() + " : ";
+	    }
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+	    Rect r = new Rect(5, 5, 800, 500);
+        GUI.Label(r, _cmdInfo);
+	}
+}
