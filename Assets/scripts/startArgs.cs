a967bd6387008831992d49e7084edecbf2cf9b97
Anthony Michelizzi <cynical89@gmail.com>
2016-11-12 16:50:47 -0800

    error checking and command parse
    - application quits if not started with any modifiers(including if it's
    just clicked)
    - application quits if the proper modifier is not used
    - application quits if the supplied argument is not the correct length
    -application quits if the supplied ID is incorrect length

--- Assets/scripts/startArgs.cs
+++ Assets/scripts/startArgs.cs
@@ -4,22 +4,45 @@ using System;
 
 public class startArgs : MonoBehaviour
 {
-    private static string _cmdInfo = string.Empty;
+    private static string _gameId = string.Empty;
+    private static string _arg = "--args-g";
 
 	// Use this for initialization
 	void Start ()
 	{
 	    string[] arguments = Environment.GetCommandLineArgs();
 
-	    foreach (string arg in arguments)
+	    if (arguments.Length <= 1)
 	    {
-	        _cmdInfo += arg.ToString() + " : ";
+            Debug.Log("No modifiers supplied");
+            Application.Quit();
+        }
+	    var cmdInfo = arguments[1];
+
+	    if (cmdInfo.Contains(_arg) == false)
+	    {
+	        Debug.Log("Incorrect Modifier supplied");
+	        Application.Quit();
+	    }
+
+	    if (cmdInfo.Length != 45)
+	    {
+	        Debug.Log("Incorrect argument supplied");
+            Application.Quit();
+	    }
+
+	    _gameId = cmdInfo.Substring(_arg.Length + 1, 36);
+
+	    if (_gameId.Length != 36)
+	    {
+	        Debug.Log("Incorrect ID supplied");
+	        Application.Quit();
 	    }
 	}
 	
 	// Update is called once per frame
 	void OnGUI () {
 	    Rect r = new Rect(5, 5, 800, 500);
-        GUI.Label(r, _cmdInfo);
+        GUI.Label(r, _gameId);
 	}
 }
